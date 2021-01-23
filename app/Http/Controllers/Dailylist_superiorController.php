18314c4c831af75b34b2585be66b83a039a46d42
hitcub <orb_pnc_ver922@yahoo.co.jp>
2021-01-24 00:43:04 +0900

    210124_01

--- app/Http/Controllers/Dailylist_superiorController.php
+++ app/Http/Controllers/Dailylist_superiorController.php
@@ -12,6 +12,12 @@ class Dailylist_superiorController extends Controller
         $tagu = '日報承認';
         $title1 = '日報承認';
         $title2 = '日報一覧';
+        $belong1 = '総務部総務課';
+        $belong2 = '営業部営業課';
+        $belong3 = '工務部工務課';
+        $name1 = '斎藤工';
+        $name2 = '妻夫木聡';
+        $name3 = '木村拓哉';
         $msgs1 = ['2021-01-07 10:00', '営業部一課', '山田太郎', '作業報告', '未承認'];
         $msgs2 = ['2021-01-06 10:00', '営業部二課', '田中治郎', '作業報告', '承認'];
         $msgs3 = ['2021-01-05 10:00', '総務部一課', '佐藤浩一', '作業報告', '未承認'];
@@ -26,6 +32,6 @@ class Dailylist_superiorController extends Controller
         $js = 'common.js';
 
         //ビューを呼び出す
-        return view('dailylist_superior', compact('tagu', 'title1', 'title2', 'msgs1', 'msgs2', 'msgs3', 'msgs4', 'msgs5', 'err_msgs1', 'err_msgs2', 'err_msgs3', 'err_msgs4', 'err_msgs5', 'css', 'js'));
+        return view('dailylist_superior', compact('tagu', 'title1', 'title2', 'belong1', 'belong2', 'belong3', 'name1', 'name2', 'name3', 'msgs1', 'msgs2', 'msgs3', 'msgs4', 'msgs5', 'err_msgs1', 'err_msgs2', 'err_msgs3', 'err_msgs4', 'err_msgs5', 'css', 'js'));
     }
 }
