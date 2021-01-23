68caab5a56bb0640c6f15e126676b7f833211488
hitcub <orb_pnc_ver922@yahoo.co.jp>
2021-01-23 17:51:09 +0900

    210123_01

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,7 +15,7 @@
     </section>
 	<section>
         <h1>@yield('title2')</h1>
-        <table border="1" class="m0a tac approval">
+        <table class="items sort-table m0a tac approval" border="1">
             <tr> 
                 <th>社員番号</th>
                 <th>所属部署</th>
@@ -24,27 +24,27 @@
                 <th>情シス</th>
             </tr>
             @if (isset($msgs1, $msgs2, $msgs3))
-                <tr> 
+                <tr class="clickable-row item"> 
                 @foreach ($msgs1 as $msg1)
                     <td>{{$msg1}}</td>
                 @endforeach
                 </tr> 
-                <tr> 
+                <t rclass="clickable-row item"> 
                 @foreach ($msgs2 as $msg2)
                     <td>{{$msg2}}</td>
                 @endforeach
                 </tr> 
-                <tr> 
+                <t rclass="clickable-row item"> 
                 @foreach ($msgs3 as $msg3)
                     <td>{{$msg3}}</td>
                 @endforeach
                 </tr>
-                <tr> 
+                <tr class="clickable-row item"> 
                 @foreach ($msgs4 as $msg4)
                     <td>{{$msg4}}</td>
                 @endforeach
                 </tr> 
-                <tr> 
+                <tr class="clickable-row item"> 
                 @foreach ($msgs5 as $msg5)
                     <td>{{$msg5}}</td>
                 @endforeach
@@ -52,4 +52,24 @@
             @endif
         </table>
 	</section>
+
+    <script>
+        $('table.items').pagination({
+            itemElement : '> > tr.item' // アイテムの要素
+        });
+    </script>
+
+    <script>
+        $(function(){
+            $('.sort-table').tablesorter({
+                textExtraction: function(node){
+                    var attr = $(node).attr('data-value');
+                    if(typeof attr !== 'undefined' && attr !== false){
+                        return attr;
+                    }
+                    return $(node).text();
+                }
+            });
+        });
+    </script>
 @endsection
